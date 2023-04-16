2808c1907244344d140e5b2093cafc8c324ff6b4
unknown <hamidforoughi.68@gmail.com>
2023-04-16 10:22:03 +0330

    remove commented codes from index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,5 @@
 const $ = document
 const navBtn = $.querySelector('.nav__btn')
-// const header = $.querySelector('.header')
 const navMenu = $.querySelector('.nav-mobile')
 const menuItemLink = $.querySelectorAll('.menu__item__link')
 let svgEelem = $.querySelectorAll('.svg-fill')
@@ -8,7 +7,6 @@ let svgEelem = $.querySelectorAll('.svg-fill')
 navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('nav__btn--open')
     navMenu.classList.toggle('nav-mobile--open')
-    // header.classList.toggle('header--hide')
 })
 
 menuItemLink.forEach(function (item) {
