ccab0aad116b642bf74ff35566a42c7fca7f0c6f
unknown <hamid.foroughi68@gmail.com>
2023-04-19 11:16:48 +0330

    input scroll design

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,22 +1,38 @@
 const $ = document
+const customScroll = $.getElementById('scroll')
 const navBtn = $.querySelector('.nav__btn')
 const navMenu = $.querySelector('.nav-mobile')
 const menuItemLink = $.querySelectorAll('.menu__item__link')
-let svgEelem = $.querySelectorAll('.svg-fill')
+const svgEelem = $.querySelectorAll('.svg-fill')
+const scrollToTop = $.querySelector('footer__scroll')
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY
+    const documentHeight = $.body.clientHeight
+    const windowHeight = window.innerHeight
+    const scrollPercent = scrollTop / (documentHeight - windowHeight)
+    const scrollPercentRounded = Math.round(scrollPercent * 100)
+    customScroll.style.width = scrollPercentRounded + '%'
+})
 
 navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('nav__btn--open')
     navMenu.classList.toggle('nav-mobile--open')
 })
 
-menuItemLink.forEach(function (item) {
+menuItemLink.forEach((item) => {
     item.addEventListener('click', () => {
         item.classList.toggle('menu__item__link--active')
     })
 })
 
-svgEelem.forEach(function (svg) {
+svgEelem.forEach((svg) => {
     svg.addEventListener('click', () => {
         svg.classList.toggle('item__icon-like--fill')
     })
 })
+
+scrollToTop.addEventListener('click', () => {
+    window.scrollTo(0, 0)
+})
+
