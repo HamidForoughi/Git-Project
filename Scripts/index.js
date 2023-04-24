84fb51227ff92067e7ba6a0b277cd59d0426c20c
unknown <hamid.foroughi68@gmail.com>
2023-04-24 16:00:20 +0330

    fix some bugs

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,19 +1,8 @@
 const $ = document
-const customScroll = $.getElementById('scroll')
 const navBtn = $.querySelector('.nav__button')
 const navMenu = $.querySelector('.nav-mobile')
 const menuItemLink = $.querySelectorAll('.menu__item__link')
 const svgEelem = $.querySelectorAll('.svg-fill')
-const scrollToTop = $.querySelector('footer__scroll')
-
-window.addEventListener('scroll', () => {
-    const scrollTop = window.scrollY
-    const documentHeight = $.body.clientHeight
-    const windowHeight = window.innerHeight
-    const scrollPercent = scrollTop / (documentHeight - windowHeight)
-    const scrollPercentRounded = Math.round(scrollPercent * 100)
-    customScroll.style.width = scrollPercentRounded + '%'
-})
 
 navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('nav__btn--open')
@@ -30,9 +19,5 @@ svgEelem.forEach((svg) => {
     svg.addEventListener('click', () => {
         svg.classList.toggle('item__icon-like--fill')
     })
-})
-
-scrollToTop.addEventListener('click', () => {
-    window.scrollTo(0, 0)
 })
 
