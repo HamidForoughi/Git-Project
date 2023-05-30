21d61d54b86f07a425983d57428943f18a6c55ce
hamidforoughi <hamidforoughi.work@gmail.com>
2023-05-30 15:26:58 +0330

    fix registration codes

--- Scripts/funcs/auth.js
+++ Scripts/funcs/auth.js
@@ -1,10 +1,9 @@
-const register = () => {
-  const $ = document;
-  const nameInput = $.getElementById('name');
-  const usernameInput = $.getElementById('username');
-  const emailInput = $.getElementById('email');
-  const phoneInput = $.getElementById('phone');
-  const passwordInput = $.getElementById('password');
+const handleRegistrationFormSubmit = async () => {
+  const nameInput = document.getElementById('name');
+  const usernameInput = document.getElementById('username');
+  const emailInput = document.getElementById('email');
+  const phoneInput = document.getElementById('phone');
+  const passwordInput = document.getElementById('password');
 
   const newUserInfos = {
     name: nameInput.value.trim(),
@@ -15,40 +14,45 @@ const register = () => {
     confirmPassword: passwordInput.value.trim()
   };
 
-  fetch(`http://localhost:3000/register`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(newUserInfos)
-  })
-  .then((res) => {
-    if(res.status === 201) {
-      return res.json(); // return the response in the next then block
-    } else if (res.status === 409) {
-      throw new Error("نام کاربری یا ایمیل قبلا استفاده شده"); // throw an error to be caught in the next catch block
-    } else {
-      throw new Error("خطایی رخ داده است، لطفا دوباره تلاش کنید"); // throw an error to be caught in the next catch block
-    }
-  })
-  .then((result) => {
-    swal({
-      title: "ثبت نام با موفقیت انجام شد",
-      icon: 'success',
-      buttons: 'ورود به پنل'
-    }).then(() => {
-      // redirect the user to the panel after successful registration
-      window.location.href = "/panel";
+  if (!newUserInfos.name || !newUserInfos.username || !newUserInfos.email || !newUserInfos.password) {
+    new swal({
+      title: 'لطفا تمامی فیلدهای مورد نیاز را پر کنید',
+      icon: 'warning',
+      buttons: 'تأیید'
     });
-  })
-  .catch((err) => {
-    // show the error message to the user
-    swal({
+    return;
+  }
+
+  try {
+    const response = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUserInfos)
+    });
+
+    if (response.status === 201) {
+      const result = await response.json();
+      new swal({
+        title: 'ثبت نام با موفقیت انجام شد',
+        icon: 'success',
+        buttons: 'ورود به پنل'
+      }).then(() => {
+        location.replace('/panel'); // redirect the user to the panel after successful registration
+      });
+    } else if (response.status === 409) {
+      throw new Error('نام کاربری یا ایمیل قبلا استفاده شده');
+    } else {
+      throw new Error('خطایی رخ داده است، لطفا دوباره تلاش کنید');
+    }
+  } catch (err) {
+    new swal({
       title: err.message,
       icon: 'error',
       buttons: 'تصحیح اطلاعات'
     });
-  });
+  }
 };
 
-export { register };
+export { handleRegistrationFormSubmit };
--- Scripts/funcs/register.js
+++ Scripts/funcs/register.js
@@ -1,8 +1,8 @@
-import { register } from './auth.js';
+import { handleRegistrationFormSubmit } from './auth.js';
 
 const registerBtn = document.getElementById('register-btn');
 
 registerBtn.addEventListener('click', event => {
     event.preventDefault();
-    register();
+    handleRegistrationFormSubmit();
 });
